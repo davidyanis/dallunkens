23cd2bf048b3b54f6df6c0909454b6d69ca5d46c
Peter Cavar <pcavar@Peters-MacBook-Pro.local>
2018-10-18 17:58:09 +0200

    commit

--- style.js
+++ style.js
@@ -1,16 +1,18 @@
+$(document).ready(function(){
+
+    $(".buttonStyleLogIn").click(function(){
+        $('.ui.modal').modal('show');      
+    });
+});
 
 function menuButtonClicked() {
     var header = document.getElementById("header");
-    header.classList.toggle("fullscreen")
-}
-
-function scrollToTop() {
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    });
-}
-
-$('.ui.modal')
-  .modal('show')
-  ;
+        header.classList.toggle("fullscreen")
+    }
+            
+    function scrollToTop() {
+        window.scroll({
+         top: 0,
+            behavior: "smooth",
+        });
+    }
