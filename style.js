8dd51b2d66e74268a5d901d289049eb3c8577eb5
David Yan <daveddd97@hotmail.com>
2018-10-29 21:30:48 +0100

    .

--- style.js
+++ style.js
@@ -9,39 +9,47 @@
 var shouldPrintWrapperOne = true
 var shouldPrintWrapperTwo = true
 
-    $(document).ready(function() {
-    $('#we-make-impact-header').hide().fadeIn(2000)
+$(document).ready(function(){
+    $('.home-wide-background').hide().delay(5000).fadeIn(2000)
 });
 
 $('.video-player').ready(function() {
     $('.video-player').delay(4000).fadeOut(1000)
 }); 
 
+    $(document).ready(function() {
+        $('.main-content-section-one').hide().transition('slide right', '500ms')
+        $('.main-content-section-two').hide().transition('slide right', '500ms')
+        $('#missonAndVisonText').hide().fadeIn(1500)
+        $('#innovationText').hide().fadeIn(1500)
+        $('#more').hide().fadeIn(2000)
+        $('#wireless').hide().fadeIn(2000)
+    });
 
     
     $(window).on('scroll', function() {
 
-        if($(this).scrollTop() > ($('.two-box-wrapper').offset().top) - 700 && shouldPrintWrapperOne) {
+        /* if($(this).scrollTop() > ($('.two-box-wrapper').offset().top) - 700 && shouldPrintWrapperOne) {
 
-            $('.main-content-section-one').hide().transition('slide right', '1000ms')
-            $('.main-content-section-two').hide().transition('slide right', '1000ms')
+            $('.main-content-section-one').hide().transition('slide right', '500ms')
+            $('.main-content-section-two').hide().transition('slide right', '500ms')
             $('#missonAndVisonText').hide().fadeIn(1500)
             $('#innovationText').hide().fadeIn(1500)
             $('#more').hide().fadeIn(2000)
             $('#wireless').hide().fadeIn(2000)
             console.log($(window).height());
-            shouldPrintWrapperOne = !shouldPrintWrapperOne
+            shouldPrintWrapperOne = !shouldPrintWrapperOne */
             /* this is the first wrapper */
     /* 
             $('.over-footer-wrapper').hide().transition('slide right', '3000ms')
             $('.over-footer-text-software').hide().fadeIn(1000)
             $('.over-footer-text-hardware').hide().fadeIn(1000)
             $('.over-footer-text-flexibility').hide().fadeIn(1000) */
-        } 
+        /* }  */
 
         if ($(this).scrollTop() > ($('.two-box-wrapper-2').offset().top) - 700 && shouldPrintWrapperTwo) {
-            $('.main-content-section-three').hide().transition('slide right', '1000ms')
-            $('.main-content-section-four').hide().transition('slide right', '1000ms')
+            $('.main-content-section-three').hide().transition('slide right', '500ms')
+            $('.main-content-section-four').hide().transition('slide right', '500ms')
             $('#coreValuesText').hide().fadeIn(1500)
             $('#jobOpeningsText').hide().fadeIn(1500)
             $('#security').hide().fadeIn(2000)
