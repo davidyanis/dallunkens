0189d1efe0aaa8038e31247f3c0bae574e48bd7c
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2018-11-20 09:37:13 +0100

    Reset branch

---
+++ style.js
@@ -0,0 +1,63 @@
+/* $(document).ready(function(){
+
+    $(".buttonStyleLogIn").click(function(){
+        $('.ui.modal').modal('show');      
+    });
+    
+}); */
+
+var shouldPrintWrapperOne = true
+var shouldPrintWrapperTwo = true
+
+/* $(document).ready(function(){
+    $('.above-main-section').hide().delay(5000).fadeIn(2000)
+});
+
+$('.video-player').ready(function() {
+    $('.video-player').delay(4000).fadeOut(1000)
+});  */
+
+
+     /* Dropdown menu on hover */
+$(document).ready(function() {
+    $('nav li ul').hide().removeClass('fallback');
+    $('nav li').mouseover(function () {
+        /* Change animation speed here slideDown(speed) */
+        $('ul', this).stop().slideDown(300);
+    });
+    $('nav li').mouseleave(function() {
+        $('ul', this).stop().slideUp(300);
+        });
+
+        /* shows and hides divs depending on which you click on */
+        $('.specification-btn').click(function() {
+            $('.specification').toggle( "fast")
+                $('.key-features').hide("fast")
+                $('.data-sheet').hide("fast")
+                // Animation complete.
+        });
+
+        $('.key-features-btn').click(function() {
+            $('.key-features').toggle( "fast")
+                $('.specification').hide("fast")
+                $('.data-sheet').hide("fast")
+                // Animation complete.
+        });
+
+    $('.data-sheet-btn' ).click(function() {
+        $('.data-sheet' ).toggle( "fast")
+            $('.specification').hide("fast")
+            $('.key-features').hide("fast")
+            // Animation complete.
+    });
+});
+
+/* Arrow up scrolling to top smooth animation */
+    function scrollToTop() {
+        window.scroll({
+         top: 0,
+            behavior: "smooth",
+        });
+    }
+     
+    
