b9a0cf5bf8d99c5820829a17fcdaeeb40600557a
David Yan <daveddd97@hotmail.com>
2018-12-17 14:42:19 +0100

    press

--- js/jquery.js
+++ js/jquery.js
@@ -70,53 +70,16 @@ $(document).ready(function() {
       });
 
      $(".press-description").hide();
-             //animation done
-             $('.ui.dropdown')
-             .dropdown()
-           ;
 
-//       // Hide Header on on scroll down
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $('header').outerHeight();
-
-// $(window).scroll(function(){
-//     didScroll = true;
-// });
-
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//     }
-// }, 250);
-
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
-    
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
-      
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (st > lastScrollTop && st > navbarHeight){
-//         // Scroll Down
-//         $('header').removeClass('nav-down').addClass('nav-up');
-        
-//     } else {
-//         // Scroll Up
-//         if(st + $(window).height() < $(document).height()) {
-//             $('header').removeClass('nav-up').addClass('nav-down');
-//         }
-//     }
-    
-//     lastScrollTop = st;
-// }
-
+     // Contact us choose input
+        $('.ui.dropdown')
+            .dropdown();
 
     
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+     });
+
 
 });
      
