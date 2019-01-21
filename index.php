50d97cdc81cd3845f084d9adc3462b0bfb481cdf
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-01-21 13:09:54 +0100

    creating php include for header and footer

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title >Sweratel AB</title>
       <link rel="shortcut icon" type="image/png" href="./images/Sweratel-logo.png"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- CSS-->
       <link rel="stylesheet" type="text/css" href="./css/footer.css">
       <link rel="stylesheet" type="text/css" href="./css/header.css">
       <link rel="stylesheet" type="text/css" href="./css/home-page.css">
       <link rel="stylesheet" href="./semantic/semantic.css">
       <!-- Fonts-->
-
       <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
       <!-- Javascript, jQuery -->
@@ -23,140 +22,125 @@
    </head>
    <body>
 
-<?php include 'header.php';   ?>
-
-
-   <main>
-      <div class="home-wide-bg">
-         <h1 class="h1-bg">Sweratels Secure Public Wi-Fi</h1>
-         <p class="description-bg">Preparing the future IT-Infrastructure</p>
-         <a href="company.php">
+      <?php include 'header.php';   ?>
+      
+      <main>
+         <div class="home-wide-bg">
+            <h1 class="h1-bg">Sweratels Secure Public Wi-Fi</h1>
+            <p class="description-bg">Preparing the future IT-Infrastructure</p>
+            <a href="company.php">
                <div class="ui big fade button wide-text-btn">
-                     Our role in shaping the future
-                  </div>
-                  </a>
-      </div>
-   </main>
-   
-
-   <!-- six cards -->
-   <div class="six-cards" style="background-color: #f2f2f2">
-   <div class="ui grid container cards-container">
-         
-         <div class="ui stackable three link cards">
-            <div class="ui blue card">
-               <div class="image">
-                  <img src="./images/in-new-york-city-1316639_1920.jpg">
+                  Our role in shaping the future
                </div>
-               <div class="content">
-                  <a href="#ewfewfew" class="header">Core Values</a>
-                  <div class="description">
-                     <p>&rdquo;Privacy is Crucial&rdquo;</p>
+            </a>
+         </div>
+      </main>
+      <!-- six cards -->
+      <div class="six-cards" style="background-color: #f2f2f2">
+         <div class="ui grid container cards-container">
+            <div class="ui stackable three link cards">
+               <div class="ui blue card">
+                  <div class="image">
+                     <img src="./images/in-new-york-city-1316639_1920.jpg">
+                  </div>
+                  <div class="content">
+                     <a href="#ewfewfew" class="header">Core Values</a>
+                     <div class="description">
+                        <p>&rdquo;Privacy is Crucial&rdquo;</p>
+                     </div>
                   </div>
                </div>
-            
-            </div>
-         
-            <div class="ui blue card">
-               <div class="image">
-                  <img src="./images/urban-438393_1920.jpg">
-               </div>
-               <div class="content">
-                  <a class="header">Mission and Vison</a>
-                  <div class="description">
-                     <p>&rdquo;The right to be Connected&rdquo;</p>
+               <div class="ui blue card">
+                  <div class="image">
+                     <img src="./images/urban-438393_1920.jpg">
+                  </div>
+                  <div class="content">
+                     <a class="header">Mission and Vison</a>
+                     <div class="description">
+                        <p>&rdquo;The right to be Connected&rdquo;</p>
+                     </div>
                   </div>
                </div>
-            </div>
-            <div class="ui blue card">
-               <div class="image">
-                  <img src="./images/tower-2723874_1920.jpg">
-               </div>
-               <div class="content">
-                  <a class="header">Software</a>
-                  <div class="description">
-                     <p> Quality innovation makes us flexibel and ready for the future.</p>
+               <div class="ui blue card">
+                  <div class="image">
+                     <img src="./images/tower-2723874_1920.jpg">
+                  </div>
+                  <div class="content">
+                     <a class="header">Software</a>
+                     <div class="description">
+                        <p> Quality innovation makes us flexibel and ready for the future.</p>
+                     </div>
                   </div>
                </div>
-            </div>
-
-            <div class="ui blue card">
-               <div class="image">
-                  <img src="./images/children.jpg">
-               </div>
-               <div class="content">
-                  <a class="header"> Heterogenoussolution</a>
-                  <div class="description">
-                     <p> &rdquo;See how we make an impact across the world, with solar Energy.&rdquo;</p>
+               <div class="ui blue card">
+                  <div class="image">
+                     <img src="./images/children.jpg">
+                  </div>
+                  <div class="content">
+                     <a class="header"> Heterogenoussolution</a>
+                     <div class="description">
+                        <p> &rdquo;See how we make an impact across the world, with solar Energy.&rdquo;</p>
+                     </div>
                   </div>
                </div>
-            </div>
-
-            <div class="ui blue card">
-               <div class="image">
-                  <img src="./images/weareHiring.jpg">
-               </div>
-               <div class="content">
-                  <a class="header">Become a Partner</a>
-                  <div class="description">
-                     <p>Join us in the task of securing the Internet-Tsunami coming towards us.</p>
+               <div class="ui blue card">
+                  <div class="image">
+                     <img src="./images/weareHiring.jpg">
+                  </div>
+                  <div class="content">
+                     <a class="header">Become a Partner</a>
+                     <div class="description">
+                        <p>Join us in the task of securing the Internet-Tsunami coming towards us.</p>
+                     </div>
                   </div>
                </div>
-            </div>
-
-            <div class="ui blue card">
-               <div class="image">
-                  <img src="./images/city-1149931_1920.jpg">
-               </div>
-               <div class="content">
-                  <a class="header">Read our blog</a>
-                  <div class="description">
-                     <p>See how we make an impact across the world.</p>
+               <div class="ui blue card">
+                  <div class="image">
+                     <img src="./images/city-1149931_1920.jpg">
+                  </div>
+                  <div class="content">
+                     <a class="header">Read our blog</a>
+                     <div class="description">
+                        <p>See how we make an impact across the world.</p>
+                     </div>
                   </div>
                </div>
             </div>
-
          </div>
       </div>
-
-   </div>
       <div class="wide-text-content">
          <h1> Sweratel combining flexible, robust and efficient solutions for worldwide customers.</h1>
          <h3> Our edge technology has improved our customers view on quality Public-WiFi & industrial IT-Solutions. The Data-Tsunami heading our way demands new technology to increase user experience without decreasing quality, security or pace.
          </h3>
          <a href="contactus.php">
-               <div class="ui compact menu">
-                  <div class="ui simple dropdown item">
-                        Request Showcase/Demo
-                     <i class="dropdown icon"></i>
-                     <div class="menu">
-                        <div class="item">Outdoor-WiFi</div>
-                        <div class="item">Indoor-WiFi</div>
-                        <div class="item">Integrated Billing</div>
-                        <div class="item">Become a Partner</div>
-                     </div>
+            <div class="ui compact menu">
+               <div class="ui simple dropdown item">
+                  Request Showcase/Demo
+                  <i class="dropdown icon"></i>
+                  <div class="menu">
+                     <div class="item">Outdoor-WiFi</div>
+                     <div class="item">Indoor-WiFi</div>
+                     <div class="item">Integrated Billing</div>
+                     <div class="item">Become a Partner</div>
                   </div>
-                  </div>
-               </a>
+               </div>
+            </div>
+         </a>
       </div>
       <div class="three-cards-row" style="background-color: #f2f2f2">
-      <div class="ui grid container special cards">
+         <div class="ui grid container special cards">
             <div class="ui three stackable cards margin-fix-cards">
-               
                <div class="ui card" style="background-color: #f2f2f2">
-                     <div class="content" style="padding-left: 0!important;">
-                        <a class="header" class="underlined-animation">Flexibility</a>
-                     </div>
-                              <div class="image">
-                              <img src="/images/night-1149700_1920.jpg">
-                           </div>
-                           
-                        
-                     <div class="description" style=" margin-top: 1em;">
-                        <p>&rdquo;Everybody should be connected, simple.&rdquo;</p>
-                     </div>
+                  <div class="content" style="padding-left: 0!important;">
+                     <a class="header" class="underlined-animation">Flexibility</a>
                   </div>
-
+                  <div class="image">
+                     <img src="/images/night-1149700_1920.jpg">
+                  </div>
+                  <div class="description" style=" margin-top: 1em;">
+                     <p>&rdquo;Everybody should be connected, simple.&rdquo;</p>
+                  </div>
+               </div>
                <div class="ui card" style="background-color: #f2f2f2">
                   <div class="content" style=" padding-left: 0!important;">
                      <a class="header">Sweratel IT-Solutions</a>
@@ -168,7 +152,6 @@
                      <p>&rdquo;Connecting more than just people&rdquo;</p>
                   </div>
                </div>
-               
                <div class="ui card" style="background-color: #f2f2f2">
                   <div class="content" style="padding-left: 0!important;">
                      <a class="header">Security</a>
@@ -176,14 +159,15 @@
                   <div class="image">
                      <img src="./images/in-new-york-city-1316639_1920.jpg">
                   </div>
-               <div class="description" style=" margin-top: 1em;">
-                  <p> Innovativ, Flexible and ready for the future.</p>
-               </div>
+                  <div class="description" style=" margin-top: 1em;">
+                     <p> Innovativ, Flexible and ready for the future.</p>
+                  </div>
                </div>
             </div>
          </div>
       </div>
 
       <?php include 'footer.php'; ?>
+
    </body>
 </html>
--- press.php
+++ press.php
@@ -26,50 +26,7 @@
       <!-- Hold space behind fixed navbar -->
       <div class="place-holder"></div>
       
-      <header id="header" class="nav-down">
-            <div class="logo-nav-wrapper">
-                  <a class="nav-logo" href="index.php"><img id="Logo" src="./images/SwratelLogo.png"></a>
-                  <i id="exit-button" class="bars big icon menu-button" onclick="menuButtonClicked()"></i>
-               </div>
-         <nav >
-            <ul>
-               <li><a href="index.php" class="hvr-overline-from-center">Home</a></li>
-               <li>
-                  <a href="company.php" class="hvr-overline-from-center">Company </a>
-               </li>
-               <li>
-                  <a href="#" class="hvr-overline-from-center">Technology <i class="chevron down icon arrow-down"></i></a>
-                  <ul class="fallback">
-                     <li><a href="RNMS.php">RNMS</a></li>
-                     <li><a href="SCM.php">SCM</a></li>
-                     <li><a href="CRM.php">CRM</a></li>
-                     <li><a href="OLS.php">OLS</a></li>
-                     <li><a href="Het-Net.php">Het-Net</a></li>
-                  </ul>
-               </li>
-               <li>
-                  <a href="#" class="hvr-overline-from-center">RAN <i class="chevron down icon arrow-down"></i></a>
-                  <ul class="fallback">
-                     <li><a href="BS.php">BS</a></li>
-                     <li><a href="EBS.php">EBS</a></li>
-                     <li><a href="IBS.php">IBS</a></li>
-                     <li><a href="MNC.php">MNC</a></li>
-                  </ul>
-               </li>
-               <li>
-                  <a href="#" class="hvr-overline-from-center">Antennas <i class="chevron down icon arrow-down"></i></a>
-                  <ul class="fallback">
-                     <li><a href="AX2.4.php">AX2.4</a></li>
-                     <li><a href="AX5.php">AX5</a></li>
-                     <li><a href="BH2.4.php">BH2.4</a></li>
-                     <li><a href="BH5.php">BH5</a></li>
-                  </ul>
-               </li>
-               <li><a href="press.php" class="hvr-overline-from-center">Press</a></li>
-               <li><a href="contactus.php" class="hvr-overline-from-center">Contact us</a></li>
-            </ul>
-         </nav>
-      </header>
+      <?php include 'header.php'; ?>
 
       <div class="press-container">
             <h3 class="ui block header">
@@ -143,67 +100,7 @@
          </div>
       </div>
    
-      <footer>
-            <div class="move-footer-center">
-               <div class="footer-wrapper">
-                  <div class="footer-logo">
-                     <a class="footer-logo-size" href="index.php"><img src="./images/sweratel-logo-png.png"></a>
-                     <div class="footer-links-contact">
-                        <li><a href="contactus.php" class="underlined-animation">Become our Partner </a></li>
-                        <li><a href="contactus.php" class="underlined-animation">Request Showcase </a></li>
-                        <li><a href="contactus.php" class="underlined-animation">Contact us</a></li>
-                     </div>
-                  </div>
-                  <div class="footer-links">
-                     <h3>Company</h3>
-                     <li><a href="#" class="underlined-animation">Heterogenoussolution </a></li>
-                     <li><a href="#" class="underlined-animation">Infrastructure </a></li>
-                     <li><a href="#" class="underlined-animation">Request demo </a></li>
-                     <li><a href="#" class="underlined-animation">Delivery time </a></li>
-                     <li><a href="#" class="underlined-animation">Investor relations </a></li>
-                  </div>
-                  <div class="footer-links">
-                     <h3>Technology</h3>
-                     <li><a href="#" class="underlined-animation">Software </a></li>
-                     <li><a href="#" class="underlined-animation">Security</a></li>
-                     <li><a href="#" class="underlined-animation">Privacy </a></li>
-                     <li><a href="#" class="underlined-animation">Maintainance </a></li>
-                     <li><a href="#" class="underlined-animation">CellularWiFi </a></li>
-                  </div>
-                  <div class="footer-links">
-                     <h3>Products</h3>
-                     <li><a href="#" class="underlined-animation">Portfolio </a></li>
-                     <li><a href="#" class="underlined-animation">Antennas </a></li>
-                     <li><a href="#" class="underlined-animation">Hardware </a></li>
-                     <li><a href="#" class="underlined-animation">Security </a></li>
-                     <li><a href="#" class="underlined-animation">Contact us </a></li>
-                  </div>
-                  <div class="adressInfo">
-                     <h3>Adress</h3>
-                     <p>Kindgrensgatan 15 <br>554 74 Jönköping <br>Sweden</p>
-                  </div>
-                  <div class="socialMediaIcons">
-                     <button class="ui linkedin button">
-                        <a href="https://www.linkedin.com/company/sweratel-ab/?trk=top_nav_home" target="_blank">
-                        <i class="linkedin icon"></i>
-                           LinkedIn
-                        </a>
-                     </button>
-                     <button class="ui twitter button twitter-button">
-                        <a href="https://twitter.com/sweratel" target="_blank">
-                        <i class="twitter icon"></i>
-                        Twitter
-                      </button>
-                                         
-                  </div>
-               </div>
-            </div>
-            <div class="copyRight">
-                  <a id="back2Top" title="Back to top" href="#"><i class="chevron circle up icon"></i></a>
-      <!-- sitemap, om oss, kontakta oss, investorrelationer, request demo system-->
-      <h6>All rights reserved © 2018 | Sweratel AB</h6>
-   </div>
-</footer>
+   <?php include 'footer.php'; ?>
  
    </body>
 </html>
