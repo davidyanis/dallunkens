19a4a7770f73cf6b46bc61ad5f437c14afe780a9
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-02-01 10:38:09 +0100

    pUsh

--- js/jquery.js
+++ js/jquery.js
@@ -108,7 +108,7 @@ $('.shape')
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 2500,
+        duration: 10000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now).toLocaleString('dot'));
